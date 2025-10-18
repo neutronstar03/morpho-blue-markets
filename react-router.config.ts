f43fc3813593416f72bae2923bcf967fb636f487
neutronstar03 <neutronstar03@protonmail.com>
2025-10-19 05:47:07 +0700

    fixing SPA prerender and basename on RR7

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  prerender: true,
+  basename: '/morpho-blue-markets',
+  prerender: ['/'],
 } satisfies Config
