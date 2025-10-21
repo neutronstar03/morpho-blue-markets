270eb83547fa62a7c148f995575ab40b4fbaa2d9
neutronstar03 <neutronstar03@protonmail.com>
2025-10-22 06:02:27 +0700

    added walletconnect for mobile

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -1,7 +1,7 @@
 import { defineChain } from 'viem'
 import { createConfig, http } from 'wagmi'
 import { arbitrum, base, katana, mainnet, polygon, unichain } from 'wagmi/chains'
-import { injected } from 'wagmi/connectors'
+import { injected, walletConnect } from 'wagmi/connectors'
 
 const hyperEvm = defineChain({
   id: 999,
@@ -32,6 +32,15 @@ export const config = createConfig({
   chains: [arbitrum, base, katana, mainnet, polygon, unichain, hyperEvm],
   connectors: [
     injected(),
+    walletConnect({
+      projectId: '0d13744a3dd855198cde1538ca87976d',
+      metadata: {
+        name: 'Morpho Blue Markets',
+        description: 'Morpho Blue Markets dApp',
+        url: 'https://neutronstar03.github.io/morpho-blue-markets/',
+        icons: ['https://neutronstar03.github.io/morpho-blue-markets/favicon.ico'],
+      },
+    }),
   ],
   transports: {
     [mainnet.id]: http('https://eth.llamarpc.com'),
