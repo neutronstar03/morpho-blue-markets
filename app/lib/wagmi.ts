2bdb8af56afa5727a031be0619f861b7b5a2cd90
neutronstar03 <neutronstar03@protonmail.com>
2025-10-22 06:08:59 +0700

    reconfiguring rainbowkit / wagmi for mobile

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -1,7 +1,7 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { defineChain } from 'viem'
-import { createConfig, http } from 'wagmi'
+import { http } from 'wagmi'
 import { arbitrum, base, katana, mainnet, polygon, unichain } from 'wagmi/chains'
-import { injected, walletConnect } from 'wagmi/connectors'
 
 const hyperEvm = defineChain({
   id: 999,
@@ -28,20 +28,10 @@ const hyperEvm = defineChain({
   },
 })
 
-export const config = createConfig({
+export const config = getDefaultConfig({
+  appName: 'Morpho Blue Markets',
+  projectId: '0d13744a3dd855198cde1538ca87976d',
   chains: [arbitrum, base, katana, mainnet, polygon, unichain, hyperEvm],
-  connectors: [
-    injected(),
-    walletConnect({
-      projectId: '0d13744a3dd855198cde1538ca87976d',
-      metadata: {
-        name: 'Morpho Blue Markets',
-        description: 'Morpho Blue Markets dApp',
-        url: 'https://neutronstar03.github.io/morpho-blue-markets/',
-        icons: ['https://neutronstar03.github.io/morpho-blue-markets/favicon.ico'],
-      },
-    }),
-  ],
   transports: {
     [mainnet.id]: http('https://eth.llamarpc.com'),
     [base.id]: http('https://1rpc.io/base'),
